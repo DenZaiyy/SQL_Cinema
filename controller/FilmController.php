5872b60a37f3547c76184455e02d9169ac33b9ec
DenZaiyy <denzaiyy@outlook.fr>
2023-03-15 16:38:13 +0100

    Adding file upload to "uploads" dir on public

--- controller/FilmController.php
+++ controller/FilmController.php
@@ -62,19 +62,27 @@ class FilmController
         $dao = new DAO();
         $db = $dao->getBDD();
 
+        $target_dir = "public/img/uploads/"; //chemin ou l'image va être upload
+        $target_file = $target_dir . uniqid() . "-" . basename($_FILES['picture']['name']); //chemin du fichier avec le nom contenant un id unique + extension du fichier
+        $file_tmp = $_FILES['picture']['tmp_name']; //le nom temporaire qui va être utiliser pour l'upload sur le serveur
+        $uploadOk = 1; //initialisation du statu d'upload à 1
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION)); //permet de récuperer l'extension du fichier à upload
+
         if (isset($_POST['submit'])) {
             $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $date = $_POST['date'];
             $duration = filter_input(INPUT_POST, "duration", FILTER_VALIDATE_INT);
-            $note = filter_input(INPUT_POST, "note", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
             $synopsis = filter_input(INPUT_POST, "synopsis", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $note = filter_input(INPUT_POST, "note", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+            $picture = $target_file;
             $director = $_POST['directors'];
+
             $genre = $_POST['id_genre'];
 
-            if ($title && $date && $duration && $synopsis && $note && $director && $genre) {
+            if ($title && $date && $duration && $synopsis && $note && $picture && $director && $genre) {
 
-                $sql = "INSERT INTO film (title, date_release, duration, synopsis, note, id_director)
-                    VALUES (:title, :date, :duration, :synopsis, :note, :director)";
+                $sql = "INSERT INTO film (title, date_release, duration, synopsis, note, picture, id_director)
+                    VALUES (:title, :date, :duration, :synopsis, :note, :picture, :director)";
 
                 $sql2 = 'INSERT INTO movie_genre (id_film, id_genre)
                          VALUES (LAST_INSERT_ID(), :id_genre)';
@@ -85,6 +93,7 @@ class FilmController
                     'duration' => $duration,
                     'synopsis' => $synopsis,
                     'note' => $note,
+                    'picture' => $picture,
                     'director' => $director
                 ];
 
@@ -92,13 +101,54 @@ class FilmController
                     'id_genre' => $genre
                 ];
 
+                $check = getimagesize($_FILES["picture"]["tmp_name"]); //vérifie si le fichier est une vrai image ou non
+                if ($check !== false) {
+                    echo "File is an image - " . $check["mime"] . ".<br>";
+                    $uploadOk = 1;
+                } else {
+                    echo "File is not an image.<br>";
+                    $uploadOk = 0;
+                }
+
+                // Check if file already exists
+                if (file_exists($target_file)) {
+                    echo "Sorry, file already exists.";
+                    $uploadOk = 0;
+                }
+
+                // Check file size
+                if ($_FILES["picture"]["size"] > 200000) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                }
+
+                // Allow certain file formats
+                if (
+                    $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    && $imageFileType != "svg"
+                ) {
+                    echo "Sorry, only JPG, JPEG, PNG & SVG files are allowed.<br>";
+                    $uploadOk = 0;
+                }
+
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded.<br>";
+                    // if everything is ok, try to upload file
+                } else {
+                    if (move_uploaded_file($file_tmp, $target_file)) {
+                        echo "The file " . htmlspecialchars(basename($_FILES["picture"]["name"])) . " has been uploaded.";
+                    } else {
+                        echo "Sorry, there was an error uploading your file.<br>";
+                    }
+                }
+
                 $addFilm = $dao->executeRequest($sql, $params);
-                $addGenre = $dao->executeRequest($sql2, $params2);
 
                 $id = $db->lastInsertId();
                 $this->detailFilm($id);
 
-                require 'view/film/detailFilm.php';
+                $addGenre = $dao->executeRequest($sql2, $params2);
             }
         } else {
             header('Location: index.php');
--- view/film/addFilm.php
+++ view/film/addFilm.php
@@ -5,7 +5,7 @@ ob_start();
 
 <div class="uk-section uk-section-secondary" style="min-height: 90vh;">
     <div class="uk-container">
-        <form action="index.php?action=addFilm" method="post" class="uk-grid-small" uk-grid>
+        <form enctype="multipart/form-data" action="index.php?action=addFilm" method="post" class="uk-grid-small" uk-grid>
             <legend class="uk-legend">Add new film</legend>
             <div class="uk-margin-small uk-width-1-1">
                 <input class="uk-input" type="text" name="title" placeholder="Title of movie" aria-label="Title of movie" required>
@@ -23,17 +23,19 @@ ob_start();
                 <textarea class="uk-textarea" rows="5" name="synopsis" placeholder="Synopsis of movie" aria-label="Synopsis of movie"></textarea>
             </div>
             <div class="uk-margin-small uk-width-1-1" uk-form-custom="target: true">
-                <input type="file" class="uk-width-1-1" name="picture" id="picture" aria-label="Custom controls">
+                <input type="file" class="uk-width-1-1" name="picture" aria-label="Custom controls">
                 <input class="uk-input uk-form-width-medium uk-width-1-1" type="text" placeholder="Select file" aria-label="Custom controls" disabled>
             </div>
             <div class="uk-margin-small uk-width-1-1">
                 <select name="directors" class="uk-select">
+                    <option value="">Choose director</option>
                     <?php foreach ($directors as $director) {
                         echo "<option value='" . $director['id_director'] . "'>" . $director['lastname'] . " " . $director['firstname'] . "</option>";
                     } ?>
                 </select>
             </div>
             <div class="uk-margin-small uk-width-1-1">
+                <label for="default"><input type="checkbox" name="default" value="">Nothing</label>
                 <?php foreach ($genres as $genre) { ?>
                     <label class="uk-margin-small-left"><input class="uk-checkbox" type="checkbox" name="id_genre" value="<?= $genre['id_genre'] ?>"> <?= $genre['label'] ?></label>
                 <?php } ?>
